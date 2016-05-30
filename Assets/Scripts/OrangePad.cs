8b1db084d00ce7731a4bc9900f063f43ce5fe7da
Weloadin <mail@weloadin.com>
2016-05-30 20:07:14 +0530

    Prototype
    Alpha V1.0

---
+++ Assets/Scripts/OrangePad.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrangePad : MonoBehaviour {
+
+    public enum OrangePadState
+    {
+        Idle,
+        Move,
+        ReachGoal,
+        Lose
+    };
+
+    private OrangePadState currentState;
+    private int padSpeed = 2 ;
+
+
+    protected void OnDestroy()
+    {
+        
+
+
+    }
+
+    void Awake()
+    {
+        currentState = OrangePadState.Idle;                
+    }
+
+
+
+    void Update()
+    {
+        switch(currentState)
+        {
+            case OrangePadState.Idle:
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        currentState = OrangePadState.Move;
+                    }
+                }
+                break;
+            case OrangePadState.Move:
+                {
+                    this.transform.position -= this.transform.up * padSpeed * Time.deltaTime;
+                }
+                break;
+            case OrangePadState.ReachGoal:
+                {
+
+                }
+                break;
+            case OrangePadState.Lose:
+                { }
+                break;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("RotateTrigger"))
+        {
+            this.transform.rotation = other.GetComponentInParent<Transform>().rotation;
+        }
+    }
+}
---
+++ Assets/Scripts/RotatingPad.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingPad : MonoBehaviour {
+
+    public enum RotatingPadState
+    {
+        RotateUp,
+        RotateRight,
+        RotateDown,
+        RotateLeft
+    };
+
+    public RotatingPadState ChangeStateTo;
+	
+	void OnMouseDown()
+    {
+        switch(ChangeStateTo)
+        {
+            case RotatingPadState.RotateUp:
+                {
+                    Quaternion newRotation = Quaternion.Euler(0, 0, 180);
+                    this.transform.rotation = newRotation;
+                    ChangeStateTo = RotatingPadState.RotateRight;
+                }
+                break;
+            case RotatingPadState.RotateRight:
+                {
+                    Quaternion newRotation = Quaternion.Euler(0, 0, 90);
+                    this.transform.rotation = newRotation;
+                    ChangeStateTo = RotatingPadState.RotateDown;
+                }
+                break;
+            case RotatingPadState.RotateDown:
+                {
+                    Quaternion newRotation = Quaternion.Euler(0, 0, 0);
+                    this.transform.rotation = newRotation;
+                    ChangeStateTo = RotatingPadState.RotateLeft;
+                }
+                break;
+            case RotatingPadState.RotateLeft:
+                {
+                    Quaternion newRotation = Quaternion.Euler(0, 0, 270);
+                    this.transform.rotation = newRotation;
+                    ChangeStateTo = RotatingPadState.RotateUp;
+                }
+                break;
+        }
+    }
+}                                                                   
